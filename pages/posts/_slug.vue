b11a8b4e181418847286a3726e2484fb290c7267
Telemet <telemetappb@gmail.com>
2020-07-22 03:11:19 +0300

    Update content template

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="post bg-gray-900">
     <article class="container mx-auto py-10 px-6 max-w-screen-md">
+      <!-- Post Image -->
+      <img
+        :src="post.img"
+        :alt="post.alt"
+        class="block w-full h-full object-cover"
+      />
+
       <!-- Post TOC -->
       <nav>
         <ul>
@@ -29,18 +36,16 @@
         {{ post.description }}
       </h2>
 
-      <hr class="mt-12 border-gray-700" />
-      <!-- Post Image -->
-      <!-- <img :src="post.img" :alt="post.alt" /> -->
+      <hr class="mt-10 md:mt-12 border-gray-700" />
 
       <!-- Post Meta -->
       <div class="flex">
         <author
           :author="post.author"
-          class="py-6 font-bold text-2xl border-l border-gray-700 pl-8"
+          class="py-4 md:py-6 pl-6 md:pl-8 font-bold text-xl md:text-2xl border-l border-gray-700"
         />
-        <p class="py-6 pr-8 text-gray-600 text-2xl">
-          {{ formatDate(post.updatedAt) }}
+        <p class="py-4 md:py-6 pr-6 md:pr-8 text-gray-500 text-xl md:text-2xl">
+          {{ formatDate(post.createdAt) }}
         </p>
       </div>
 
@@ -71,8 +76,39 @@ export default {
 }
 
 .content {
+  @apply tracking-wide;
+  & h2 {
+    @apply mt-12 text-4xl text-red-600 font-bold;
+    @screen md {
+      @apply mt-16 text-5xl;
+    }
+  }
   & p {
-    @apply text-gray-400 my-6 text-2xl tracking-wide;
+    @apply text-gray-400 my-4 text-xl;
+    @screen md {
+      @apply my-6 text-2xl;
+    }
+  }
+  & blockquote p {
+    @apply pr-6 py-4 my-8 text-2xl text-gray-400 border-r-4 border-red-600 font-bold;
+    @screen md {
+      @apply pr-8 py-6 my-12 text-3xl border-r-8;
+    }
+  }
+  & ol {
+    @apply list-none px-4 my-12 text-2xl;
+    counter-reset: my-counter;
+    & li {
+      @apply flex my-8;
+      counter-increment: my-counter;
+      &::before {
+        @apply ml-4 text-gray-600 font-bold;
+        content: counter(my-counter) '';
+        /* content: counter(my-counter) '. '; */
+        /* color: red; */
+        /* font-weight: bold; */
+      }
+    }
   }
 }
 </style>
